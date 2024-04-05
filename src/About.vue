56a2bc380c8436fa3bfb3c4f98d47231bfb4ac0d
tokisakininovn <trieuduytancbg@gmail.com>
2024-04-05 23:26:39 +0700

    #1

---
+++ src/About.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>About</h1>
+        <p>This is the about page</p>
+    </div>
+</template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,15 +6,16 @@
             </div>
             <nav>
                 <ul>
-                    <li><a href="/"><ion-icon name="home-outline"></ion-icon>Home</a></li>
-                    <li><a href="/products"><ion-icon name="person-outline"></ion-icon>Profile</a></li>
-                    <li><a href="/services"><ion-icon name="albums-outline"></ion-icon>Gallery</a></li>
-                    <li><a href="/contact"><ion-icon name="information-circle-outline"></ion-icon>About</a></li>
+                    <li><a href="/"><ion-icon name="home-outline"></ion-icon></a></li>
+                    <li><a href="/products"><ion-icon name="person-outline"></ion-icon></a></li>
+                    <li><a href="/services"><ion-icon name="albums-outline"></ion-icon></a></li>
+                    <li><a href="/contact"><ion-icon name="information-circle-outline"></ion-icon></a></li>
                 </ul>
             </nav>
     
             <div class="tool_navbar">
-                <ul>
+                <ion-icon name="settings-outline"></ion-icon>
+                <ul class="hidden">
                     <li><ion-icon name="settings-outline"></ion-icon>Setting</li>
                     <li><ion-icon name="moon-outline"></ion-icon>Darklight</li>
                     <li><ion-icon name="language-outline"></ion-icon>Language</li>
