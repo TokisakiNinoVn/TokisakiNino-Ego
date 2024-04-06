50addd2bec2e143355510ff036a81700ebb3856a
tokisakininovn <trieuduytancbg@gmail.com>
2024-04-07 01:13:26 +0700

    build: main

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,12 @@
   <main>
     <div class="main_content">
       <div class="banner">
+        <img class="logoHI3" src="../public/img/logogame.png" alt="Logo HI3">
+        <div class="infor_basic">
+          <span class="fullname">Elysia</span>
+          <span class="descript">"Just as predicted in the book, “Paradise does not exist”. If heaven doesn't exist then I will create it myself."</span>
+          <span class="herrcher"><ion-icon name="sparkles-outline"></ion-icon> Herrscher of Human: Ego <ion-icon name="sparkles-outline"></ion-icon></span>
+        </div>
         <img class="elysia_main" src="../public/ElysiaAlbum/ElysiaMaid.png" alt="">
       </div>
     </div>
