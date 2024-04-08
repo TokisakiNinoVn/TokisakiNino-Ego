c872cd7b6912322599662be991b88388b47cc5ac
tokisakininovn <trieuduytancbg@gmail.com>
2024-04-08 17:18:49 +0700

    build: add images

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,46 @@
   <main>
     <div class="main_content">
       <div class="banner">
-        <img class="logoHI3" src="../public/img/logogame.png" alt="Logo HI3">
+        <img class="logoHI3" src="/img/logogame.png" alt="Logo HI3">
         <div class="infor_basic">
           <span class="fullname">Elysia</span>
           <span class="descript">"Just as predicted in the book, “Paradise does not exist”. If heaven doesn't exist then I will create it myself."</span>
           <span class="herrcher"><ion-icon name="sparkles-outline"></ion-icon> Herrscher of Human: Ego <ion-icon name="sparkles-outline"></ion-icon></span>
         </div>
-        <img class="elysia_main" src="../public/ElysiaAlbum/ElysiaMaid.png" alt="">
+        <img class="elysia_main" src="/ElysiaAlbum/ElysiaMaid.png" alt="">
+      </div>
+      <div class="bottom_container">
+        <div class="art_box">
+          <span>Arts</span>
+          <div class="art_container">
+            <img src="/ElysiaAlbum/Elysia(3).jpg" alt="">
+            <img src="/ElysiaAlbum/Elysia(3).jpg" alt="">
+          </div>
+        </div>
+        <button>Viewmore</button>
+        <div class="search_box">
+          <span>Search keywords</span>
+          <div class="input_search">
+            <ion-icon name="search-outline"></ion-icon>
+            <input type="text" placeholder="Herrscher of Human: Ego">
+          </div>
+          <div class="input_search">
+            <ion-icon name="search-outline"></ion-icon>
+            <input type="text" placeholder="Herrscher of Human: Ego">
+          </div>
+          <div class="input_search">
+            <ion-icon name="search-outline"></ion-icon>
+            <input type="text" placeholder="Herrscher of Human: Ego">
+          </div>
+          <div class="input_search">
+            <ion-icon name="search-outline"></ion-icon>
+            <input type="text" placeholder="Herrscher of Human: Ego">
+          </div>
+        </div>
+        <div class="artboxt_wiu">
+          <img src="/ElysiaAlbum/ElyIcon(2).png" alt="">
+          <img src="/ElysiaAlbum/ElyIcon(2).png" alt="">
+        </div>
       </div>
     </div>
   </main>
