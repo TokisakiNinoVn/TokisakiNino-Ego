a10c1d284f8b0fb3f9638b4e4ab6ecd08a78d24a
tokisakininovn <trieuduytancbg@gmail.com>
2024-04-09 20:16:26 +0700

    feat: Add script tags for main.js in about.html, gallery.html, and index.html

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 // import './assets/css/main.css'
 // import './assets/css/style.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-createApp(App).mount('body')
 
+
+// Hàm kiểm tra độ rộng trình duyệt và xóa phần tử body nếu cần
+function checkScreenWidth() {
+    const screenWidth = window.innerWidth || document.documentElement.clientWidth;
+
+    if (screenWidth < 1140) {
+        document.body.remove();
+    }
+}
+
+// Kiểm tra độ rộng trình duyệt khi trang web được tải và cũng mỗi khi kích thước cửa sổ thay đổi
+window.addEventListener('DOMContentLoaded', checkScreenWidth);
+window.addEventListener('resize', checkScreenWidth);
 
