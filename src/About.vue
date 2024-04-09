bff8d5a1c41335ba6a07709b5b0104e079d48df6
tokisakininovn <trieuduytancbg@gmail.com>
2024-04-09 16:55:25 +0700

    build: about content

--- src/About.vue
+++ src/About.vue
@@ -8,26 +8,27 @@ import Header from './components/Header.vue'
             <Header />
         </header>
         <div class="main_about">
-            <span>A few things I know about "Ego"</span>
+            <span class="title_about">A few things I know about "Ego"</span>
             <p class="first_para">
                 The term "Ego" is often used to refer to a part of the human mind, representing self-awareness, pride, and arrogance. In psychology, the "ego" is commonly understood as a part of the self, playing a role in mediating between the conscious and unconscious mind, and aiding adaptation to the external world. A strong ego can lead to confidence and independence, while a weak ego can result in confidence and susceptibility to harm. specific, "ego" is often associated with Sigmund Freud's concept of the three parts of the mind (id, ego, and superego), wherein "ego" acts as the intermediary, balancing the primal desires of the "id" with the societal norms of the "superego". 
             </p>
             <p class="second_para">
                 Certainly! In Sigmund Freud's psychoanalytic theory, "id" and "superego" alongside "ego" form the fundamental trio of the mind.
                 <span>
-                    1. **Id**: The "Id" represents the unconscious part of the mind, containing human's primal desires and instincts such as pleasure, gratification, and reproduction. It is not bound by any societal rules or limitations and operates on the principle of "I want what I want when I want it."
+                    1. Id: The "Id" represents the unconscious part of the mind, containing human's primal desires and instincts such as pleasure, gratification, and reproduction. It is not bound by any societal rules or limitations and operates on the principle of "I want what I want when I want it."
                 </span>
                 <span>
-                    2. **Superego**: The "Superego" represents the conscious part of the mind containing societal rules, values, and standards. It develops through learning from family, society, and culture, and helps restrain and shape behavior according to moral principles and laws.
+                    2. Superego: The "Superego" represents the conscious part of the mind containing societal rules, values, and standards. It develops through learning from family, society, and culture, and helps restrain and shape behavior according to moral principles and laws.
                 </span>
                 <span>
-                    3. **Ego**: The "Ego" serves as the intermediary between the "id" and "superego," tasked with regulating and balancing the primal needs of the "id" with the societal norms of the "superego." The ego is the part of the mind that operates based on the principles of reality and practicality, striving to fulfill the needs of the "id" in a safe and socially acceptable manner.
+                    3. Ego: The "Ego" serves as the intermediary between the "id" and "superego," tasked with regulating and balancing the primal needs of the "id" with the societal norms of the "superego." The ego is the part of the mind that operates based on the principles of reality and practicality, striving to fulfill the needs of the "id" in a safe and socially acceptable manner.
                 </span>
             </p>
-
+            
             <p class="third_para">
-                "Master of the Human: Ego" could be translated as "Authority of the Human: Ego." In this context, "Ego" can be understood as a part of the human self, representing self-awareness and consciousness, as well as as well as arrogance. This title may imply the autonomy, confidence, and willpower of humans, manifested through the strength and authority of the "ego." In some cases, this could imply the self-possession, initiative, and self-determination of individuals in their lives, not overly influenced by external factors.
-                <img src="/ElysiaAlbum/ElyIcon(5).png" alt="Elysia Icon">
+                "Herrscher of Human: Ego" could be translated as "Authority of the Human: Ego." In this context, "Ego" can be understood as a part of the human self, representing self-awareness and consciousness, as well as as well as arrogance. This title may imply the autonomy, confidence, and willpower of humans, manifested through the strength and authority of the "ego." In some cases, this could imply the self-possession, initiative, and self-determination of individuals in their lives, not overly influenced by external factors.
+                <!-- <img src="../public/ElysiaAlbum/Ely_Album.jpg" alt="Elysia Icon"> -->
+                <!-- <img src="/ElysiaAlbum/ElyIcon(5).png" alt="Elysia Icon"> -->
             </p>
         </div>
     </main>
